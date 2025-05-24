6531ee87f03282a3dccc37fc32c7b1c5ec02831d
M.AB <raphaelbholmes@gmail.com>
2025-05-24 19:37:22 -0300

    Modelo contato

---
+++ AgendaApp.Server/DTOs/ContatoDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace AgendaApp.Server.DTOs
+{
+    public class ContatoDTO
+    {
+        public required int Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Email { get; set; }
+
+        public required string Telefone { get; set; }
+    }
+}
